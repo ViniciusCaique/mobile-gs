4bac467d2e41f82c771d8fc859677cd545f092ca
Vinicius Caique <viniciuscaiquesilva@gmail.com>
2023-11-24 22:05:57 -0300

    final commit

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
 
+// rm95367 - Enrico Lopes
+// rm95628 - Fernando Cesar 
+// rm94698 - Pedro Poline 
+// rm93550 - Vinicius Caique
+
 import { Routes } from './src/routes';
 
 export default function App() {
